2b028d86c13612b7a1746a6a94fd98d60a073ade
Shresth-013 <shresthraj12004@gmail.com>
2026-05-22 23:59:59 +0530

    Song Change

--- src/MusicToggle.jsx
+++ src/MusicToggle.jsx
@@ -1,25 +1,69 @@
 import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 
-// We use a free soft piano loop from a public CDN
-const MUSIC_URL = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+const HAPPY_BIRTHDAY = [
+  [261.63,0.35],[261.63,0.15],[293.66,0.5],[261.63,0.5],[349.23,0.5],[329.63,1.0],
+  [261.63,0.35],[261.63,0.15],[293.66,0.5],[261.63,0.5],[392.00,0.5],[349.23,1.0],
+  [261.63,0.35],[261.63,0.15],[523.25,0.5],[440.00,0.5],[349.23,0.5],[329.63,0.5],[293.66,1.0],
+  [466.16,0.35],[466.16,0.15],[440.00,0.5],[349.23,0.5],[392.00,0.5],[349.23,1.2],
+];
+
+function playHappyBirthday(audioCtxRef, sourceNodesRef, onEnd) {
+  const ctx = new (window.AudioContext || window.webkitAudioContext)();
+  audioCtxRef.current = ctx;
+  sourceNodesRef.current = [];
+
+  let time = ctx.currentTime + 0.05;
+
+  HAPPY_BIRTHDAY.forEach(([freq, dur]) => {
+    const osc = ctx.createOscillator();
+    const gain = ctx.createGain();
+
+    osc.type = "sine";
+    osc.frequency.setValueAtTime(freq, time);
+
+    gain.gain.setValueAtTime(0, time);
+    gain.gain.linearRampToValueAtTime(0.22, time + 0.02);
+    gain.gain.setValueAtTime(0.22, time + dur - 0.05);
+    gain.gain.linearRampToValueAtTime(0, time + dur);
+
+    osc.connect(gain);
+    gain.connect(ctx.destination);
+
+    osc.start(time);
+    osc.stop(time + dur);
+    sourceNodesRef.current.push(osc);
+
+    time += dur + 0.06;
+  });
+
+  const lastOsc = sourceNodesRef.current[sourceNodesRef.current.length - 1];
+  lastOsc.onended = () => {
+    ctx.close();
+    onEnd();
+  };
+}
 
 export default function MusicToggle() {
   const [playing, setPlaying] = useState(false);
-  const audioRef = useRef(null);
+  const audioCtxRef = useRef(null);
+  const sourceNodesRef = useRef([]);
+
+  const stop = () => {
+    if (audioCtxRef.current) {
+      audioCtxRef.current.close();
+      audioCtxRef.current = null;
+    }
+    setPlaying(false);
+  };
 
   const toggle = () => {
-    if (!audioRef.current) {
-      audioRef.current = new Audio(MUSIC_URL);
-      audioRef.current.loop = true;
-      audioRef.current.volume = 0.18;
-    }
     if (playing) {
-      audioRef.current.pause();
+      stop();
     } else {
-      audioRef.current.play().catch(() => {});
+      setPlaying(true);
+      playHappyBirthday(audioCtxRef, sourceNodesRef, () => setPlaying(false));
     }
-    setPlaying(!playing);
   };
 
   return (
@@ -45,7 +89,7 @@ export default function MusicToggle() {
         justifyContent: "center",
         transition: "box-shadow 0.3s",
       }}
-      title={playing ? "Pause music" : "Play soft music"}
+      title={playing ? "Stop music" : "Play Happy Birthday"}
     >
       {playing ? "🔊" : "🎵"}
     </motion.button>
